74921ee9b048de6739271b9e05c6d48a7ebf325e
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2022-04-25 12:32:35 +0100

    genrate message  classes

---
+++ src/main/java/de/tekup/soap/cons/LoanEligBConsApplication.java
@@ -0,0 +1,13 @@
+package de.tekup.soap.cons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanEligBConsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanEligBConsApplication.class, args);
+	}
+
+}
